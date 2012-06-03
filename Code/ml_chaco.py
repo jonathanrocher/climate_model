7166e8af39b90af9e331ec25a33eab1ab772e4f1
Deepankar Sharma <deepankarsharma@gmail.com>
2012-06-03 15:58:20 -0400

    Fixed padding issues.

--- Code/ml_chaco.py
+++ Code/ml_chaco.py
@@ -15,11 +15,16 @@ def gen_scatter_plot(series_one, series_two):
 		y=series_two.data())
 	plot = Plot(pd)
 	scatterplot = plot.plot(('x','y'), color='lightblue',
-		type='scatter', marker='circle')[0]
+		type='scatter', marker='circle', marker_size=2)[0]
 
 	plot.x_axis.title = series_one.label()
 	plot.x_axis.tick_visible = False
+	plot.x_axis._draw_labels = lambda x: 1
+	plot.x_axis.title_spacing = 0.25
 
 	plot.y_axis.title = series_two.label()
 	plot.y_axis.tick_visible = False
+	plot.y_axis._draw_labels = lambda x: 1
+	plot.y_axis.title_spacing = 0.25
+	plot.padding = [18, 18, 18, 18]
 	return plot
